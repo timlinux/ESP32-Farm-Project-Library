f41f4cb8b453e0d760b50827cfac3d3a7cd42fab
Tim Sutton <tim@kartoza.com>
2022-08-30 00:17:15 +0100

    Added header file

---
+++ ESP32-Farm-Project-Library.cpp
@@ -0,0 +1,16 @@
+/*************************************************
+ *
+ *  ESP32-Farm-Project-Library
+ *
+ *  License: MIT
+ *
+ *  Author: Tim Sutton
+ *
+ *  This library accompanies the ESP32-Farm-Project
+ *                        see 
+ *  https://github.com/timlinux/ESP32-Farm-Project
+ *
+ *  IoT project using ESP32, MQTT for managing my 
+ *  farm water and other systems.
+ *
+ * ***********************************************/
---
+++ ESP32-Farm-Project-Library.h
@@ -0,0 +1,40 @@
+/*************************************************
+ *
+ *  ESP32-Farm-Project-Library
+ *
+ *  License: MIT
+ *
+ *  Author: Tim Sutton
+ *
+ *  This library accompanies the ESP32-Farm-Project
+ *                        see 
+ *  https://github.com/timlinux/ESP32-Farm-Project
+ *
+ *  IoT project using ESP32, MQTT for managing my 
+ *  farm water and other systems.
+ *
+ * ***********************************************/
+
+#ifndef ESP32_FARM_PROJECT_LIBRARY_H 
+#define ESP32_FARM_PROJECT_LIBRARY_H 
+
+// Sensor types
+
+#define SENSOR_TEMPERATURE String('T')
+#define SENSOR_SOIL_MOISTURE String('M')
+#define SENSOR_GATE String('G')
+#define SENSOR_CAMERA String('C')
+#define SENSOR_HUMIDITY String('H')
+#define SENSOR_SOUND String('S')
+
+#define MODE_INSTRUCTION String('I')
+#define MODE_RESPONSE String('R')
+
+#define DEVICE_MASTER String('M')
+#define DEVICE_PUMPHOUSE String('P')
+
+#define DEVICE_TYPE_HELTEC String('Heltec ESP LoRA WiFi')
+
+
+
+#endif  // ESP32_FARM_PROJECT_LIBRARY_H
